2d6d85dda123cd6937b20a1ec31e1c3690ad2cb1
motters <sammottley@gmail.com>
2017-11-03 20:35:54 +0000

    Fixed issue with 'Illegal instruction' error when running 20_mount_sysroot.sh

--- 20_mount_sysroot.sh
+++ 20_mount_sysroot.sh
@@ -22,8 +22,11 @@ sudo mount -o bind /proc $SYSROOT/proc
 sudo mount -o bind /dev $SYSROOT/dev
 sudo mount -o bind /sys $SYSROOT/sys
 
+sudo sed -i '/./s/^/#/g' $SYSROOT/etc/ld.so.preload
+
 #sudo chroot $SYSROOT /bin/bash -c 'apt-get update'
 #sudo chroot $SYSROOT /bin/bash -c 'rpi-update'
+sudo chroot $SYSROOT /bin/bash -c 'uname -a'
 sudo chroot $SYSROOT /bin/bash -c 'apt install -y libfontconfig1-dev libfreetype6-dev libx11-dev libxext-dev libxfixes-dev libxi-dev libxrender-dev libxcb1-dev libx11-xcb-dev libxcb-glx0-dev'
 sudo chroot $SYSROOT /bin/bash -c 'apt install -y libxcb1 libxcb1-dev libx11-xcb1 libx11-xcb-dev libxcb-keysyms1 libxcb-keysyms1-dev libxcb-image0 libxcb-image0-dev libxcb-shm0 libxcb-shm0-dev libxcb-icccm4 libxcb-icccm4-dev libxcb-sync1 libxcb-sync-dev libxcb-render-util0 libxcb-render-util0-dev libxcb-xfixes0-dev libxrender-dev libxcb-shape0-dev libxcb-randr0-dev libxcb-glx0-dev libxcb-xinerama0-dev'
-popd
+popd 
